d6cc237a8b102e1e4418fedb804ad01e1bd287df
Jeffrey Lin <jeffrey82221@gmail.com>
2024-11-09 13:08:07 +0800

    feature: enhance sdk

--- sdk/requests_recorder.py
+++ sdk/requests_recorder.py
@@ -1,3 +1,10 @@
+"""
+應該要有一個關聯排序功能：
+
+把endpoint與網頁輸入的網址做比對
+從最相關排到最不相關
+"""
+
 from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -5,16 +12,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 from seleniumwire.utils import decode
 
 
-
 driver = webdriver.Chrome(
     service=Service(executable_path=ChromeDriverManager().install()),
 )
-result = driver.get("https://www.cnyes.com/")
+result = driver.get("https://findbiz.nat.gov.tw/fts/query/QueryCmpyDetail/queryCmpyDetail.do")
 for i, request in enumerate(driver.requests):
     if request.response:
-        print(f'REQUEST No.{i}', request.response.status_code, request.method, request.response.headers['Content-Type'])
-        print(request.url)
-        body = decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity'))
-        print('Body Size:', len(body))
-        print('=================================================')
+        if request.response.headers['Content-Type'] not in [
+                'image/gif', 'text/javascript'
+            ] and 'google' not in request.url and 'facebook' not in request.url:
+            print(f'REQUEST No.{i}', request.response.status_code, request.method, request.response.headers['Content-Type'])
+            print(request.url)
+            body = decode(request.response.body, request.response.headers.get('Content-Encoding', 'identity'))
+            print('Body Size:', len(body))
+            print('=================================================')
 driver.close()
