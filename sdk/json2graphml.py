459977d4f7835f955a577c96fd899c669ddc7a46
Jeffrey Lin <jeffrey82221@gmail.com>
2024-11-10 13:19:45 +0800

    feature: complete json2graphml

--- sdk/json2graphml.py
+++ sdk/json2graphml.py
@@ -1,12 +1,30 @@
-from yaml2dot import data_loader,converter
+from yaml2dot import data_loader
+from yaml2dot.renderer import render
+import networkx as nx
+import io
+
+def rm_visual_info(G: nx.Graph) -> nx.Graph:
+    """
+    移除 dot 檔中視覺化的資訊
+    """
+    node_link_data = nx.node_link_data(G, edges='edges')
+    node_link_data['graph'] = {}
+    for node in node_link_data['nodes']:
+        for key in ['fontname', 'fontsize', 'margin', 'fillcolor', 'penwidth', 'style', 'shape']:
+            del node[key]
+    for link in node_link_data['edges']:
+        for key in ['arrowhead', 'penwidth']:
+            del link[key]
+    return nx.node_link_graph(node_link_data, edges="edges")
 
 # Load YAML or JSON data from a file
-data = data_loader.load_yaml_or_json('../examples/cnyes/funds/jsons/HoldingsCrawler.json')
-# or input a python dict directly
-# data = {"example":"example"}
+data = data_loader.load_yaml_or_json('../examples/cnyes/funds/jsons/nav.json')
+G = render(data)
+G = rm_visual_info(G)
+buff = io.StringIO()
+nx.write_network_text(G, buff)
+buff.seek(0)
+print('Networkx Visualization:')
+print(buff.read())
+nx.write_graphml_xml(G, 'example.graphml')
 
-# Convert the data to DOT format (default)
-dot_output = converter.convert_yaml_or_json_to_format(data)
-# Print or save the outputs as needed
-with open('example.dot', 'w') as f:
-    f.write(dot_output)
